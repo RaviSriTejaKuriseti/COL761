8163e8a0884bc462c36353bcdce73b1bac40ec52
KURISETI RAVI SRI TEJA <pandukuriseti@gmail.com>
2022-09-19 18:50:27 +0530

    Add files via upload

---
+++ Assignment-1/scripts/cs5190471_install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+git clone https://github.com/RaviSriTejaKuriseti/Data-Mining-Assignments.git
+cd Data-Mining-Assignments
+unzip HW1_cs5190471.zip
+module load compiler/gcc/7.1.0/compilervars
+module load compiler/python/3.6.0/ucs4/gnu/447
+module load pythonpackages/3.6.0/matplotlib/3.0.2/gnu
